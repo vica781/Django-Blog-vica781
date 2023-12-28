367bd8bc5075482481047b664e62199e3fbc5685
Victoria Kalenik-Harris <vk781@hotmail.com>
2023-12-28 17:18:29 +0000

    Add localhost to allowed_hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY') # new
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
