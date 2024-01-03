8e42512081368eea39837dd0fb4fffa7a3312069
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-01-03 18:27:03 +0000

    Deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os # new
-import dj_database_url # new
-if os.path.isfile('env.py'): # new         
-    import env as env # new
+import os 
+import dj_database_url
+from django.contrib.messages import constants as messages 
+if os.path.isfile('env.py'):          
+    import env as env 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates') # new
 SECRET_KEY = os.environ.get('SECRET_KEY') # new
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['django-blog-vica781-a37921b429b8.herokuapp.com', '127.0.0.1']
 
@@ -57,6 +60,14 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
